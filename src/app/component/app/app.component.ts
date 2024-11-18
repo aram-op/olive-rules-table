31806a8725a0b3eec704b3fa8b24ef4552c2bdd9
aram-op <aram.tadevosyan@optimumpartners.co>
2024-11-18 18:21:25 +0400

    Added sidebar and navbar

--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, MatSidenavContent, MatNavList, MatListItem, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
