7e3515b24c9b347dde7989b1696aedd88a1c216c
aram-op <aram.tadevosyan@optimumpartners.co>
2024-11-19 13:40:50 +0400

    Updated balance component test suite

--- src/test/balance.component.spec.ts
+++ src/test/balance.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BalanceComponent } from '../app/component/balance/balance.component';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {By} from '@angular/platform-browser';
 
 describe('BalanceComponent', () => {
   let component: BalanceComponent;
@@ -8,16 +11,55 @@ describe('BalanceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BalanceComponent]
+      imports: [BalanceComponent, NoopAnimationsModule],
+      providers: [provideNativeDateAdapter()]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(BalanceComponent);
+    fixture.componentRef.setInput('isThroughBalance', true);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form', () => {
+    expect(component.form).toBeTruthy();
+  });
+
+  describe('.ngOnInit()', () => {
+    it('should emit the form on value changes', () => {
+      jest.spyOn(component.formValueChanged, 'emit');
+
+      component.form.controls.frequency.setValue('10');
+
+      expect(component.formValueChanged.emit).toHaveBeenCalledWith(component.form);
+    });
+  });
+
+  describe('if is through balance', () => {
+    it('should display all form controls', () => {
+      fixture.componentRef.setInput('isThroughBalance', true);
+
+      const controls = fixture.debugElement.queryAll(By.css('mat-form-field, mat-checkbox'));
+
+      expect(controls.length).toEqual(14);
+    });
+  });
+
+  describe('if is event based', () => {
+    it('should display only 6 form controls', () => {
+      fixture = TestBed.createComponent(BalanceComponent);
+      fixture.componentRef.setInput('isThroughBalance', false);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      const controls = fixture.debugElement.queryAll(By.css('mat-form-field, mat-checkbox'));
+
+      expect(controls.length).toEqual(6);
+    });
   });
 });
